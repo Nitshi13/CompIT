784ec80e40261a32ddfbfd642403037842d735a5
Yurii77777 <yuretshome@gmail.com>
2023-02-02 09:23:08 +0200

    Add peoducts line btns

--- commands/sendCatalogButton.ts
+++ commands/sendCatalogButton.ts
@@ -31,6 +31,78 @@ export const sendCatalogButton = async (ctx: any): Promise<any> => {
         url: URLS.PRODUCT_LINE_APRICOT,
       },
     ],
+    [
+      {
+        text: MESSAGES_AU.DE_LYS_BLANC_LINE,
+        url: URLS.PRODUCT_LINE_DE_LYS_BLANC,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.TRIFOLIUM_PRETENSE_LINE,
+        url: URLS.PRODUCT_LINE_TRIFOLIUM_PRETENSE,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.LANA_BLUR_LINE,
+        url: URLS.PRODUCT_LINE_LANA_BLUR,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.SMART_BIOLOGICA_COMPELEXES,
+        url: URLS.PRODUCT_LINE_SMART_BIOLOGICA_COMPELEXES,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.VEGETABLE_LINE,
+        url: URLS.PRODUCT_LINE_VEGETABLE,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.FRUIT_LINE,
+        url: URLS.PRODUCT_LINE_FRUIT,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.BODY_LINE,
+        url: URLS.PRODUCT_LINE_BODY,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.PRO_PEELS_LINE,
+        url: URLS.PRODUCT_LINE_PRO_PEELS,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.PRO_PEELS_LINE,
+        url: URLS.PRODUCT_LINE_APPARAT,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.APPARAT_LINE,
+        url: URLS.PRODUCT_LINE_APPARAT,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.ALMASK_LINE,
+        url: URLS.PRODUCT_LINE_ALMASK,
+      },
+    ],
+    [
+      {
+        text: MESSAGES_AU.RE_CELL_LINE,
+        url: URLS.PRODUCT_LINE_RE_CELL,
+      },
+    ],
   ];
 
   return await ctx.reply(`${MESSAGES_AU.CATALOG_PARAGRAPH}`, Markup.inlineKeyboard(keyboard));
--- constants/urls.ts
+++ constants/urls.ts
@@ -4,4 +4,18 @@ export const URLS = {
   PROTOCOL: 'https://drive.google.com/file/d/1oa2zcpV6EbSWJhNnq2yyo43S8MPWiJdl/view?usp=share_link',
   PRODUCT_LINE_INULA: 'https://drive.google.com/file/d/1XLR6hyZHaNyP8juHqgzsXVtalcXcsItA/view?usp=share_link',
   PRODUCT_LINE_APRICOT: 'https://drive.google.com/file/d/1ZjoEgdzttgfz7_nbWyjcO48QxFyZ8MrI/view?usp=share_link',
+  PRODUCT_LINE_DE_LYS_BLANC: 'https://drive.google.com/file/d/18cTa9J9U_I1gXxy49RtkMqViQTxHKEWM/view?usp=share_link',
+  PRODUCT_LINE_TRIFOLIUM_PRETENSE:
+    'https://drive.google.com/file/d/1G028qEDWgNuS2MfBJk4lzoGNMX5TWbca/view?usp=share_link',
+  PRODUCT_LINE_LANA_BLUR: 'https://drive.google.com/file/d/1UY31JIdeqehlwH4xjCDLTh-aabe1q0YD/view?usp=share_link',
+  PRODUCT_LINE_SMART_BIOLOGICA_COMPELEXES:
+    'https://drive.google.com/file/d/16zyVSpy2tjQbLX8io3ofDKn7Knctg9uB/view?usp=share_link',
+  PRODUCT_LINE_VEGETABLE: 'https://drive.google.com/file/d/1Tx0fPN-zCYkbEo_KJJ71ryO9ZvllqGe4/view?usp=share_link',
+  PRODUCT_LINE_FRUIT: 'https://drive.google.com/file/d/1apjjbZD6EwAhEgDRbK-rtNNbquWV2Q3n/view?usp=share_link',
+  PRODUCT_LINE_BODY: 'https://drive.google.com/file/d/1vXlVS7vV5Oh1QZZSoU4U1-wzyLKUAx_T/view?usp=share_link',
+  PRODUCT_LINE_PRO_PEELS: 'https://drive.google.com/file/d/1eVYpl5yJ3hrMuvsd4NV-k_mtzn_n8uW3/view?usp=share_link',
+  PRODUCT_LINE_APPARAT: 'https://drive.google.com/file/d/1y9Yk1U38JCcDT3KAxlLrpWgFyBN5W6tN/view?usp=share_link',
+  PRODUCT_LINE_CARBOXY: 'https://drive.google.com/file/d/1Cg44-mwpKWY-krII1bO3OXI4uPicF6RH/view?usp=share_link',
+  PRODUCT_LINE_ALMASK: 'https://drive.google.com/file/d/1iZOL0P_j5nFy0X4KHPDkvUtnsAWu_hfF/view?usp=share_link',
+  PRODUCT_LINE_RE_CELL: 'https://drive.google.com/file/d/1h50xYERwgD0dvbDUk-9qcj5bY0ZoaLip/view?usp=share_link',
 };
